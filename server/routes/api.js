8bcf0d7379e0fb78cad99dd5228ac6006ccb8189
PetchCracker <6131501035@lamduan.mfu.ac.th>
2021-10-06 03:22:46 +0700

    test pust

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,8 +7,5 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
-});
 
 module.exports = router;
