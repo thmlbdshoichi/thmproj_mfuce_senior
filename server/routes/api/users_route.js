7e7d20ac4824516d4c937269778d75e41fcfeb23
Arunwat Moonbung <thmlbdshoichi@gmail.com>
2021-10-15 15:14:08 +0700

    users_route finished

--- server/routes/api/users_route.js
+++ server/routes/api/users_route.js
@@ -3,25 +3,17 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Users = require('../../models/users_db') //Schema  
 
-/*
-Convert and simplified
-1. Updated
-2. delete
-3. Delete All
-from Petch controllers file
-*/
-
 router.get('/', (req, res, next) => {
     Users.find().exec()
-    .then(data => {res.status(200).json(data)})
-    .catch(err => {res.status(500).json({message: 'An error occurred while retrieving User.',errorDetails: err})})
+    .then(data => {data ? res.status(200).json(data) : res.status(400).json({message: `Data not found`, errorDetails: data})})
+    .catch(err => {res.status(500).json({message: 'An error occurred while retrieving User.',errorDetails: err})});
 });
 
 router.get('/:userId', (req, res, next) => {
     const userId = req.params.userId;
-    Users.findById(userId).exec()
-    .then(data => {data ? res.status(200).json(data) : res.status(401).json({message: `${userId} not found`,errorDetails: data})})
-    .catch(err => {res.status(500).json({message: 'An error occurred while finding User.',errorDetails: err})})
+    Users.findById({ _id : userId }).exec()
+    .then(data => {data ? res.status(200).json(data) : res.status(400).json({message: `${userId} not found`,errorDetails: data})})
+    .catch(err => {res.status(500).json({message: 'An error occurred while finding User.',errorDetails: err})});
 });
 
 router.post('/', (req, res, next) => {
@@ -40,5 +32,34 @@ router.post('/', (req, res, next) => {
     .then(data => {res.status(201).json({message: 'User has been successfully created',createdUser: users})})
     .catch(err => {res.status(500).json({message: 'An error occurred while creating User.',errorDetails: err})});
 });
+
+router.patch('/:userId', (req, res, next) => {
+    const userId = req.params.userId;
+    Users.findByIdAndUpdate(userId, { $set: req.body }, { new: true }).exec()
+    .then(data => data ? res.status(200).json(data) : res.status(400).json({message: `Can't Update, ${userId} doesn't exist`, errorDetails: data}))
+    .catch(err => res.status(500).json({message: 'An error occurred while updating User.',errorDetails: err}));
+});
+
+router.patch('/resDivPush/:userId', (req, res, next) => {
+    const userId = req.params.userId;
+    Users.findByIdAndUpdate(userId, { $addToSet: { resDiv: req.body.resDiv }}, { new: true }).exec()
+    .then(data => data ? res.status(200).json(data) : res.status(400).json({message: `Can't Add responsible division to, ${userId} doesn't exist`, errorDetails: data}))
+    .catch(err => res.status(500).json({message: 'An error occurred while updating responsible division to User.',errorDetails: err}));
+});
+
+router.patch('/resDivPull/:userId', (req, res, next) => {
+    const userId = req.params.userId;
+    Users.findByIdAndUpdate(userId, { $pull: { resDiv: req.body.resDiv }}, { new: true }).exec()
+    .then(data => data ? res.status(200).json(data) : res.status(400).json({message: `Can't remove responsible division to, ${userId} doesn't exist`, errorDetails: data}))
+    .catch(err => res.status(500).json({message: 'An error occurred while updating responsible division to User.',errorDetails: err}));
+});
+
+router.delete('/:userId', (req, res, next) => {
+    const userId = req.params.userId;
+    Users.findOneAndDelete({ _id : userId }).exec()
+    .then(data => data ? res.status(200).json({message: 'User has been successfully deleted',deletedUser: userId}) : res.status(400).json({message: `Can't delete, ${userId} doesn't exist`, errorDetails: data}))
+    .catch(err => res.status(500).json({message: 'An error occurred while deleting User.',errorDetails: err}));
+})
+
 
 module.exports = router;
