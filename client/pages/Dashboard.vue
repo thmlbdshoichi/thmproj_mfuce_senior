02111433b688c860c45fda3affc6603fd00ae933
PetchCracker <6131501035@lamduan.mfu.ac.th>
2021-11-04 04:18:09 +0700

    add 3 menu: เลือกหน่วยงาน,เลือกคำถาม,เลือกระยะเวลา

--- client/pages/Dashboard.vue
+++ client/pages/Dashboard.vue
@@ -1,33 +1,135 @@
 <template>
   <div>
-    <v-row align="center">
-      <h3>Division:</h3>
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="items"
-          label="Choose Division to Create"
-          dense
-          solo
-        ></v-select>
-      </v-col>
-    </v-row>
+    <v-card>
+    <v-card-title>
+      <!--  เลือกหน่วยงาน --> 
+        <v-col cols="12" sm="6" md="6" lg="6">
+        <v-icon medium color="#bc8e5d"> mdi-numeric-1-circle </v-icon>
+        เลือกหน่วยงานที่ต้องการ 
+          <v-select 
+          @change="changeSelect1" 
+          v-model="divTagIndex" 
+          @input="fetchItems()"
+          :items="items" 
+          item-text="divName" 
+          item-value="divTag" 
+          prepend-icon="mdi-magnify" 
+          single-line hide-details></v-select>
+        </v-col>
+      
+      <!--เลือกคำถาม --> 
+        <v-col cols="12" sm="6" md="6" lg="6" v-if="select1" >
+          <v-icon medium color="#bc8e5d"> mdi-numeric-2-circle </v-icon>
+          เลือกคำถามที่ต้องการ
+          <v-select 
+          @change="changeSelect2" 
+          v-model="divTagIndex" 
+          @input="fetchItems()"
+          :items="items" 
+          item-text="divName" 
+          item-value="divTag" 
+          prepend-icon="mdi-magnify" 
+          single-line hide-details></v-select>
+        </v-col>
+
+        <!--เลือกระยะเวลาที่ต้องการดู --> 
+         <!-- v-if="selected" --> 
+        <v-col cols="12" sm="6" md="6" lg="6" v-if="select2" >
+              <v-icon medium color="#bc8e5d"> mdi-numeric-3-circle </v-icon>
+            เลือกระยะเวลาที่ต้องการดู
+            <v-row>
+            <v-col cols="12" sm="6" md="6" lg="6" >
+              <v-menu
+                ref="menu1"
+                v-model="menu1"
+                :close-on-content-click="false"
+                :return-value.sync="date"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="stdate"
+                    label="Start Date"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="stdate"
+                  no-title
+                  scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn text color="#bc8e5d" @click="menu1 = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn text color="#bc8e5d" @click="$refs.menu1.save(date)" >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols="12" sm="6" md="6" lg="6" >
+              <v-menu
+                ref="menu2"
+                v-model="menu2"
+                :close-on-content-click="false"
+                :return-value.sync="date"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="eddate"
+                    label="End Date"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="eddate"
+                  no-title
+                  scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn text color="#bc8e5d" @click="menu2 = false" >
+                    Cancel
+                  </v-btn>
+                  <v-btn text color="#bc8e5d" @click="$refs.menu2.save(date)">
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-col>
+            </v-row>
+        </v-col>
+      </v-card-title>
+    </v-card>
+    <br>
 
     <v-card>
       <v-card-text>
         <v-row>
           <v-col md="6">
-            <v-card>
+            <v-card v-if="overview">
               <bar-chart
                 :barchartdata="barchartdata"
                 :barchartoptions="barchartoptions"
               />
             </v-card>
-            <v-card>
+            <!--<v-card>
               <line-chart
                 :linechartdata="chartdata"
                 :linechartoptions="options"
               />
-            </v-card>
+            </v-card>-->
           </v-col>
 
           <v-col md="6">
@@ -124,7 +226,10 @@ export default {
         ],
       },
     },
+    overview: true,
     loaded: false,
+    select1: false,
+    select2: false,
     apichartdata: {
       labels: [],
       datasets: [],
@@ -132,9 +237,23 @@ export default {
     jsonchartdata: {
       labels: [],
       datasets: [],
-    },
+    }, // script of date
+    stdate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    eddate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu1: false,
+      menu2: false,
+      modal: false,
   }),
+  
   methods: {
+    changeSelect1(selectObj) {
+        this.select1 = true;
+        this.loaded = true;
+        this.overview = false;
+     },
+     changeSelect2(selectObj) {
+        this.select2 = true;
+     },
     async fetchItems(){
       const apiURLdivs = `${process.env.AXIOS_BASEURL}/api/divs`;
       this.$axios.get(apiURLdivs).then(res => {this.divisions = res.data})
