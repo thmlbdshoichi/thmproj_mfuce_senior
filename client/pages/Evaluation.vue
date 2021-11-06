7527b901ff2cbb782afa513c25fc41df83f82d75
PetchCracker <6131501035@lamduan.mfu.ac.th>
2021-11-06 07:54:15 +0700

    สร้าง evaluation page เสร็จแล้ว

--- client/pages/Evaluation.vue
+++ client/pages/Evaluation.vue
@@ -25,7 +25,7 @@
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex lg7 md>
-          <v-card color="#F6EEE0" class="elevation-12 box-login">
+          <v-card class="elevation-12 box-login">
             <div class="header">
               <h1>Service Evaluation</h1>
             </div>
@@ -46,90 +46,131 @@
               </v-container>
             </div>
 
-            <v-card-text>
-              <v-card>
-              <div v-for="i in 5" v-if="question" class="question" >
-                <v-row align="center">
-                  <h2 class="subheading--text ">Question:</h2>
-                  
-                </v-row>
-
-              <br> 
-              <div align="center">
-                <v-btn
-                  color="#F9957F"
-                  depressed
-                  elevation="7"
-                  large
-                  rounded
-                  class="worst"
-                >
-                  WORST </v-btn
-                >
-
-                <v-btn
-                  color="#F7D7C2"
-                  depressed
-                  elevation="7"
-                  large
-                  rounded
-                  class="moderate"
-                >
-                  MODERATE
-                </v-btn>
-
-                <br /><br />
-                <v-btn
-                  color="#D4E6C4"
-                  depressed
-                  elevation="7"
-                  large
-                  rounded
-                  class="good"
-                >
-                  GOOD </v-btn
-                >
-                <v-btn
-                  color="#ABCFD1"
-                  depressed
-                  elevation="7"
-                  large
-                  rounded
-                  class="best"
-                >
-                  BEST
-                </v-btn>
+            <!--<v-card> -->
+              <v-card-text v-if="selected" align="center">
+                <div v-for="(question, index) in quiz.questions" :key="index" >
+                  <div v-show="index == questionIndex" >
+                    <h4>{{ question.qSequence }} of {{ quiz.questions.length }}</h4><br>
+                    <h2><v-icon medium color="#bc8e5d"> mdi-numeric-{{ question.qSequence }}-circle </v-icon>  
+                    {{ question.qName }}</h2>
+                      <br> 
+                      <v-card-text>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="6" lg="6">
+                              <v-btn color="#F9957F" depressed elevation="7" large rounded class="worst">
+                                WORST 
+                              </v-btn>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="6" lg="6">
+                              <v-btn color="#F7D7C2" depressed elevation="7" large rounded class="moderate">
+                                MODERATE
+                              </v-btn>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="6" lg="6">
+                              <v-btn color="#D4E6C4" depressed elevation="7" large rounded class="good">
+                                GOOD 
+                              </v-btn >
+                          </v-col>
+                          <v-col cols="12" sm="6" md="6" lg="6">
+                              <v-btn color="#ABCFD1" depressed elevation="7" large rounded class="best" >
+                                BEST
+                              </v-btn>
+                          </v-col>
+                        </v-row>
+                        <v-row >
+                          <v-col cols="12" sm="6" md="6">
+                          </v-col>
+                          <v-col cols="12" sm="6" md="6">
+                            <div class="addressmfu">
+                              <br />
+                               <v-btn v-if="questionIndex > 0" v-on:click="prev()"  class="ma-2 btn-prev" outlined rounded color="indigo">
+                                prev
+                              </v-btn>
+                              <v-btn v-on:click="next()"  class="ma-2" outlined rounded color="indigo">
+                                next
+                              </v-btn>
+                            </div>
+                          </v-col>
+                        </v-row>
+                    </v-card-text>
+                  </div>
                 </div>
-              </div>
-              </v-card>
-            </v-card-text>
-          </v-card>
+                <!-- ข้อเสนอแนะ -->
+                <div v-show=" questionIndex == quiz.questions.length" >
+                  <h2> ข้อเสนอแนะ </h2>
+                  <br>
+                    <template>
+                     <v-textarea
+                        solo
+                        name="input-7-4"
+                        label="กรุณาให้ข้อเสนอแนะ"
+                      ></v-textarea>
+                      <v-btn class="mr-4" @click="submit">
+                        submit
+                      </v-btn>
+                    </template>
+                </div>
+              </v-card-text>
+            </v-card>
+          <!--</v-card> -->
         </v-flex>
       </v-layout>
     </v-container>
 
     <v-footer dark padless class="ftcl">
-      <div class="line"></div>
-      <v-card-text>
-        <div class="namemfu">
-          <br />
-          มหาวิทยาลัยแม่ฟ้าหลวง<br />
-          Mae Fah Luang University
-        </div>
-
-        <div class="addressmfu">
-          <br />
-          333 Moo1, Thasud, Muang, Chiang Rai 57100<br />
-          Tel: 0-5391-6000, 0-5391-7034 Fax: 0-5391-6034, 0-5391-7049<br />
-          Mae Fah Luang University, Bangkok Office Tel: 0-2679-0038-9 Fax:
-          0-2679-0038 <br />
-        </div>
-      </v-card-text>
-    </v-footer>
+        <div class="line"></div>
+        <v-card-text>
+          <v-row >
+            <v-col cols="12" sm="6" md="6">
+              <div class="namemfu">
+                <br />
+                มหาวิทยาลัยแม่ฟ้าหลวง<br />
+                Mae Fah Luang University
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <div class="addressmfu">
+                <br />
+                333 Moo1, Thasud, Muang, Chiang Rai 57100<br />
+                Tel: 0-5391-6000, 0-5391-7034 Fax: 0-5391-6034, 0-5391-7049<br />
+                Mae Fah Luang University, Bangkok Office Tel: 0-2679-0038-9 Fax:
+                0-2679-0038 <br /></div
+            ></v-col>
+          </v-row>
+        </v-card-text>
+      </v-footer>
   </v-app>
 </template>
 
 <script>
+  // ทดสอบ (ตัวอย่างข้อมูล)
+  let quiz = {
+      title: 'My Quiz',
+      questions: [{
+          qSequence: 1,
+          qName: "What is your pet name?",
+          divTag: "C0",
+        },
+        {
+          qSequence: 2,
+          qName: "How old are you?",
+          divTag: "C1",
+        },
+        {
+          qSequence: 3,
+          qName: "Where did you go last year?",
+          divTag: "C2",
+        },
+        {
+          qSequence: 4,
+          qName: "Pad Thai อร่อยไหม?",
+          divTag: "C3",
+        },
+      ]
+    } //
+
 export default {
   data () {
       return {
@@ -139,14 +180,27 @@ export default {
           'Division02',
           'Division03',
         ],
-        question: false,
+        selected: false,
+        quiz: quiz,
+        questionIndex: 0,
       }
     },
   methods: {
     startQuiz () {
      // this.$store.dispatch('beginQuiz', this.Division)
-     this.question=true;
-    }
+     this.selected = true;
+    },
+
+    next (){
+      this.questionIndex ++;
+    },
+
+    prev () {
+        this.questionIndex --;
+    },
+    submit () {
+     // something ...
+    },
   }
 }
 </script>
@@ -158,16 +212,16 @@ export default {
   padding: 3%;
 }
 .worst {
-  width: 250px;
+  width: 100%;
 }
 .moderate {
-  width: 250px;
+  width: 100%;
 }
 .good {
-  width: 250px;
+  width: 100%;
 }
 .best {
-  width: 250px;
+  width: 100%;
 }
 
 .box-login {
@@ -186,6 +240,10 @@ export default {
   float: right;
   text-align: right;
   font-size: 16px;
+}
+
+.btn-prev-next {
+  align-content: right;
 }
 
 .namemfu {
